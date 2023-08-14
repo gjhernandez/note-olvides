4fbadd3da20c869c0360a767dd2b52d154a17126
Jose Hernandez <gjhernandez@uninorte.edu.co>
2023-08-14 03:33:18 +0200

    Config: Adding more ESLint configs and fixing resulting issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/eslint-module"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-  }
-})
+  },
+  typescript: {
+    strict: true,
+  },
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts',
-    './nuxt.config.{js,ts',
-    './app.vue',
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts",
+    "./nuxt.config.{js,ts",
+    "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: "#466eb2",
+          light: "#557bbc",
+        },
+        secondary: "#95cebc",
+        brand: {
+          yellow: "#fce698",
+        },
+      },
+    },
   },
-  plugins: [],
-} satisfies Config
+  plugins: [require("@tailwindcss/forms")],
+} satisfies Config;
